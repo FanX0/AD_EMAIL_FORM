b0d8736e038897ee0248b3f08ca2569725453a1c
farid azhari <faridazhari46@gmail.com>
2023-03-21 10:08:59 +0700

    Initial commit

---
+++ app/src/main/java/com/i/federicorjm201103676/emailformfaridazharinurcahyo202102335/MainActivity.java
@@ -0,0 +1,14 @@
+package com.i.federicorjm201103676.emailformfaridazharinurcahyo202102335;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
